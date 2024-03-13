3cbefb82e2c0710db649523455626f4e64928ef3
yongZin-MAC <>
2024-03-13 16:36:40 +0900

    chore: Add scroll top button

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,5 +1,5 @@
 //메인 페이지 컴포넌트
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import styled from "styled-components";
 import Modal from "../components/Modal";
 import UploadForm from "../components/UploadForm";
@@ -10,10 +10,7 @@ import Details from "../components/Details";
 import { ModalContext } from "../context/ModalContext";
 import freitagBG from "../components/images/truck.jpg";
 
-const Wrap = styled.div`
-  /* padding-top:64px; */
-`;
-const BrandInfo = styled.div`
+const BrandInfo = styled.section`
 	width:100%;
 	height:500px;
 	background:url(${freitagBG}) no-repeat;
@@ -38,17 +35,76 @@ const BrandInfo = styled.div`
 		height:180px;
 	}
 `;
+const ScrollTopBtn = styled.button`
+	width:32px;
+	height:32px;
+	border-radius:50%;
+	border:1px solid rgba(255,255,255,0.3);
+	background-color:#111;
+	position:fixed;
+	bottom:-30px;
+	right:10px;
+	opacity:0.2;
+	transition:0.2s;
+	&:before{
+		content:"";
+		width:30%;
+		height:30%;
+		border-top:2px solid #fff;
+		border-right:2px solid #fff;
+		position:absolute;
+		top:60%;
+		left:50%;
+		transform:translate(-50%, -50%) rotate(-45deg);
+	}
+	&.on{
+		bottom:20px;
+	}
+	&:hover{
+		opacity:1;
+	}
+`;
 
 const MainPage = () => {
 	const {modalView} = useContext(ModalContext);
+	const [showButton, setShowButton] = useState(false);
+
+	const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+	}, []);
+
+	const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // 부드러운 스크롤 효과 적용
+    });
+  };
 
 	return (
-		<Wrap>
+		<main>
       <BrandInfo>
         {/* 오래된 트럭 방수 천을 기본 원단으로 삼아 자전거 바퀴 속 고무, 폐차 안전벨트를 더해 가방을 만듭니다. 낡아 버려질 것들이 세상에 단 하나뿐인 실용적이고 도시적인 프라이탁 가방으로 완성됩니다. */}
       </BrandInfo>
       
       <ProductList/>
+
+			<ScrollTopBtn
+				type="button"
+				className={showButton ? "on" : ""}
+				onClick={scrollToTop}
+			/>
 
       {modalView !== "off" &&
         <Modal>
@@ -60,7 +116,7 @@ const MainPage = () => {
           }
         </Modal>
       }
-		</Wrap>
+		</main>
 	)
 }
 
