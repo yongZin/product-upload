7ac1c8e0edb96d58dfd9ea4a7502012fb3d3909c
yongZin-MAC <>
2024-05-07 21:41:35 +0900

    chore: Change footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,26 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const Copy = styled.p`
-	margin-top:30px;
+const Copy = styled.div`
+	padding:60px 0 30px;
 	font-size:14px;
-	font-family:var(--f-light);
-`;
-const Wrap = styled.footer`
-	padding:30px 20px;
-	color:#fff;
-	background-color:#333;
-	p:not(${Copy}){
-		font-size:20px;
-	}
+	text-align:center;
+	font-family:var(--f-reular);
 `;
 
 const Footer = () => {
 	return (
-		<Wrap>
-			<p>포트폴리오 - 박용진</p>
-			<Copy>Copyright © 2024 YongZin. All rights reserved.</Copy>
-		</Wrap>
+		<Copy>Copyright © 2024 YongZin. All rights reserved.</Copy>
 	)
 }
 
