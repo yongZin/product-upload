28cb5c1773e5b5dbd6da4a3e2e3e3a1b0a9be893
yongZin-MAC <>
2024-05-10 18:06:57 +0900

    chore: Add loading animaion

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -7,6 +7,7 @@ import ProductList from "../components/ProductList";
 import LoginForm from "../components/LoginForm";
 import RegisterForm from "../components/RegisterForm";
 import Details from "../components/Details";
+import Loading from "../components/Loading";
 import { ModalContext } from "../context/ModalContext";
 import freitagBG from "../components/images/truck.jpg";
 
@@ -107,6 +108,9 @@ const MainPage = () => {
 
 	return (
 		<main>
+			{loginCheck &&
+				<Loading />
+			}
       <BrandInfo>
         {/* 오래된 트럭 방수 천을 기본 원단으로 삼아 자전거 바퀴 속 고무, 폐차 안전벨트를 더해 가방을 만듭니다. 낡아 버려질 것들이 세상에 단 하나뿐인 실용적이고 도시적인 프라이탁 가방으로 완성됩니다. */}
       </BrandInfo>
