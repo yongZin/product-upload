bc324c6c14354286320a5f33783d464d021d7af1
yongZin-MAC <>
2024-06-03 22:24:29 +0900

    chore: Change server.js

--- server.js
+++ server.js
@@ -9,47 +9,23 @@ const app = express();
 const { MONGO_URI, MY_APP_PORT } = process.env;
 
 mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("MongoDB 연결");
+	.connect(MONGO_URI)
+	.then(() => {
+		console.log("MongoDB 연결");
 
-    // 빌드된 React 정적 파일 제공 설정
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
-    app.use(express.json());
-    app.use(authentication); // 유저 인증 미들웨어
-    app.use("/upload", productRouter); // "upload"가 들어가면 productRouter 실행
-    app.use("/users", userRouter); // "users"가 들어가면 userRouter 실행
-
-    // 모든 다른 요청을 React 앱으로 라우팅
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+		app.use(express.static("build")); //빌드한 경우 경로
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/build/index.html");
     });
 
-    app.listen(MY_APP_PORT, () => console.log("PORT : " + MY_APP_PORT));
-  })
-  .catch((error) => console.log(error));
+		// app.use("/uploads", express.static("uploads")); //로컬경로 사용시
+		app.use(express.json());
+		app.use(authentication); //유저인증 미들웨어
+		app.use("/upload", productRouter); //"upload"가 들어가면 productRouter 실행
+		app.use("/users", userRouter); // "users"가 들어가면 userRouter실행
 
-
-
-// mongoose
-// 	.connect(MONGO_URI)
-// 	.then(() => {
-// 		console.log("MongoDB 연결");
-
-// 		app.use(express.static("build")); //빌드한 경우 경로
-//     app.get("/", (req, res) => {
-//       res.sendFile(__dirname + "/build/index.html");
-//     });
-
-// 		// app.use("/uploads", express.static("uploads")); //로컬경로 사용시
-// 		app.use(express.json());
-// 		app.use(authentication); //유저인증 미들웨어
-// 		app.use("/upload", productRouter); //"upload"가 들어가면 productRouter 실행
-// 		app.use("/users", userRouter); // "users"가 들어가면 userRouter실행
-
-// 		app.listen(MY_APP_PORT, () => console.log("PORT : " + MY_APP_PORT))
-// 	})
-// 	.catch((error) => console.log(error));
+		app.listen(MY_APP_PORT, () => console.log("PORT : " + MY_APP_PORT))
+	})
+	.catch((error) => console.log(error));
 	// admin
 	// NqEGY6y8EuPljCn9
