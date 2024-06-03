4a6a1014c89ca1009ea03c40d1b533fa31be9997
yongZin-MAC <>
2024-06-03 23:13:56 +0900

    chore: Change server

--- server.js
+++ server.js
@@ -12,11 +12,7 @@ mongoose
 	.connect(MONGO_URI)
 	.then(() => {
 		console.log("MongoDB 연결");
-
-		app.use(express.static(path.join(__dirname, "client", "build")));
-		app.get("/*", (req, res) => {
-      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    });
+		
 		// app.use(express.static("build")); //빌드한 경우 경로
     // app.get("/", (req, res) => {
     //   res.sendFile(__dirname + "/build/index.html");
@@ -27,6 +23,11 @@ mongoose
 		app.use(authentication); //유저인증 미들웨어
 		app.use("/upload", productRouter); //"upload"가 들어가면 productRouter 실행
 		app.use("/users", userRouter); // "users"가 들어가면 userRouter실행
+
+		app.use(express.static(path.join(__dirname, "client", "build")));
+		app.get("/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
 
 		app.listen(MY_APP_PORT, () => console.log("PORT : " + MY_APP_PORT))
 	})
