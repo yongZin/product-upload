10f8ac936c3a391ac5a9a1450aba292baf1d5470
yongZin-MAC <>
2024-06-04 17:24:29 +0900

    chore: Update server 2

--- server.js
+++ server.js
@@ -13,21 +13,16 @@ mongoose
 	.then(() => {
 		console.log("MongoDB 연결");
 		
-		// app.use(express.static("build")); //빌드한 경우 경로
-    // app.get("/", (req, res) => {
-    //   res.sendFile(__dirname + "/build/index.html");
-    // });
+		app.use(express.static("build")); //빌드한 경우 경로
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/build/index.html");
+    });
 
 		// app.use("/uploads", express.static("uploads")); //로컬경로 사용시
 		app.use(express.json());
 		app.use(authentication); //유저인증 미들웨어
 		app.use("/upload", productRouter); //"upload"가 들어가면 productRouter 실행
 		app.use("/users", userRouter); // "users"가 들어가면 userRouter실행
-
-		app.use(express.static(path.join(__dirname, "client", "build")));
-		app.get("/*", (req, res) => {
-      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    });
 
 		app.listen(MY_APP_PORT, () => console.log("PORT : " + MY_APP_PORT))
 	})
